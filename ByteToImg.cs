bf658911790b6745377097610ec8867234dac788
JorgeWSantos <jorgeewillian47@gmail.com>
2019-07-05 14:24:17 -0300

    Classes de conversão de Image para Byte[] e Byte[] to Image.

---
+++ ByteToImg.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+using System.IO;
+
+namespace biom
+{
+    class ByteToImg
+    {
+        //CONVERTE DE byte[] para Image
+        private Image ByteToImage(byte[] foto)
+        {
+            using (var ms = new MemoryStream(foto))
+            {
+                return Image.FromStream(ms);
+            }
+        }
+    }
+}
---
+++ ImgToByteArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biom
+{
+    class ImgToByteArray
+    {
+        //CONVERTE DE Image para byte[]
+        private byte[] ImageToByteArray(Image img)
+        {
+            using (var stream = new MemoryStream())
+            {
+                img.Save(stream, ImageFormat.Png);
+                stream.Seek(0, SeekOrigin.Begin);
+                byte[] bArray = new byte[stream.Length];
+                stream.Read(bArray, 0, Convert.ToInt32(stream.Length));
+                return bArray;
+            }
+        }
+    }
+}
