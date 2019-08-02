de4cbb36023476ed146ecf3e5615524703670994
JorgeWSantos <jorgeewillian47@gmail.com>
2019-08-02 11:04:35 -0300

    Lógica, criptografar/Descriptografar dados e salvar no regedit

---
+++ Decrypt key - Program.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Decrypt_Registry_Key
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //hash
+            string hash = "&eg0s@so%%luçõ&s&*";
+
+            string data_decrypt = String.Empty;
+
+            //abrindo a key
+            using (RegistryKey key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\SGP"))
+            {
+                //se a key existir
+                if (key != null)
+                {
+                    //pega o value da key
+                    string key_value = key.GetValue("st").ToString();
+
+                    //caso o value esteja vazio
+                    //significa que o usuário tentou alterar o valor
+                    if (String.IsNullOrEmpty(key_value))
+                        MessageBox.Show("Sua licença expirou, entre em contato com a EGOS-TI\n para obter uma versão atualizada.", 
+                                        "", MessageBoxButtons.OK, 
+                                        MessageBoxIcon.Information);
+
+                    try
+                    {
+                        //valor base
+                        byte[] decrypt_value = Convert.FromBase64String(key_value);
+
+                        //encrypt
+                        using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+                        {
+                            byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+                            using (TripleDESCryptoServiceProvider tripDes = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                            {
+                                ICryptoTransform transform = tripDes.CreateDecryptor();
+                                byte[] results = transform.TransformFinalBlock(decrypt_value, 0, decrypt_value.Length);
+                                data_decrypt = UTF8Encoding.UTF8.GetString(results);
+                            }
+                        }
+
+                        DateTime data_expirar = Convert.ToDateTime(data_decrypt);//tenta converter os dados extraídos da regkey,
+                                                                                 //caso não converta é porque o usuário tentou alterar o hash_value da key.
+
+                        DateTime dt = Convert.ToDateTime("09/08/2019");
+
+                        //caso a data de registro da key for maior que 7 dias.
+                        if (data_expirar.AddDays(7) < dt)
+                            MessageBox.Show("Sua licença expirou, entre em contato com a EGOS-TI\n para obter uma versão atualizada.",
+                                            "", MessageBoxButtons.OK,
+                                            MessageBoxIcon.Information);
+
+                        //caso a data não tenha expirado
+                        //executa o programa.
+                        else
+                        {
+                            //executa o programa
+                        }
+                    }
+                    catch (FormatException)//caso a data seja alterada, o formato decriptado ira vir em formato errado.
+                    {
+                        Registry.LocalMachine.DeleteSubKey(@"SOFTWARE\SGP");//deleta a subkey
+
+                        MessageBox.Show("Sua licença expirou, entre em contato com a EGOS-TI\n para obter uma versão atualizada.", 
+                                        "", MessageBoxButtons.OK, 
+                                        MessageBoxIcon.Information);
+                    }
+                    catch(Exception e)
+                    {
+                        MessageBox.Show(e.Message, 
+                                        "", 
+                                        MessageBoxButtons.OK, 
+                                        MessageBoxIcon.Information);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Encrypt Key - Program.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Registry_Key_Encripted
+{
+    class Program
+    {
+        //hash usado para a criptografia
+        private static string hash = "&eg0s@so%%luçõ&s&*";
+
+        static void Main(string[] args)
+        {
+            //conferindo a existencia da key
+            using (RegistryKey key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\SGP"))
+            {
+                //se a key NÂO existir é CRIADA.
+                //caso EXISTA, NADA é ALTERADO.
+                if (key == null)
+                    EncryptKey();
+
+            }
+        }
+
+        private static void EncryptKey()
+        {
+            //string que recebe a data criptografada
+            string data_encrypt = String.Empty;
+
+            //data_atual
+            string data_atual = DateTime.Now.ToString().Substring(0, 10);
+
+            //valor base
+            byte[] encrypt_value = UTF8Encoding.UTF8.GetBytes(data_atual);
+
+            //encrypt
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+                using (TripleDESCryptoServiceProvider tripDes = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                {
+                    ICryptoTransform transform = tripDes.CreateEncryptor();
+                    byte[] results = transform.TransformFinalBlock(encrypt_value, 0, encrypt_value.Length);
+                    data_encrypt = Convert.ToBase64String(results, 0, results.Length);
+                }
+            }
+
+            //criando key
+            using (RegistryKey key = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\SGP"))
+            {
+                //setando o value da 'key'
+                key.SetValue("st", $"{data_encrypt}");
+            }
+        }
+    }
+}
